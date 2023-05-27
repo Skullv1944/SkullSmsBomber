03c97fcabd1056c4e23f7c9af3c0c090fa7ee528
Skullv1944 <132088757+Skullv1944@users.noreply.github.com>
2023-05-27 19:33:01 +0300

    Update title.js

--- modules/title.js
+++ modules/title.js
@@ -1,8 +1,8 @@
 function console_title(x) {
     if (process.platform == 'win32') {
-        process.title = x + " / FastBomber v1.0 - github.com/fastuptime";
+        process.title = x + " / SkullBomber v1.0 - Skull";
     } else {
-        process.stdout.write('\x1b]2;' + x + " / FastBomber v1.0 - github.com/fastuptime" + '\x1b\x5c');
+        process.stdout.write('\x1b]2;' + x + " / SkullBomber v1.0 - Skull" + '\x1b\x5c');
     }
 }
 
