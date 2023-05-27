58ead39fefafe924ff43f3ef16a18379b9ed4b5c
Skullv1944 <132088757+Skullv1944@users.noreply.github.com>
2023-05-27 19:27:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,21 +5,21 @@ const fastBomber = require('./modules/sms.js');
 
 title('Hosgeldiniz');
 
-let telefon = rl.question('Telefon Numarasi Giriniz +90: ' .green);
+let telefon = rl.question('Telefon Numarası Griniz +90: ' .green);
 if (telefon.length != 10) {
-    console.log('Telefon Numarasi 10 Haneli Olmalidir. Ex: 5401234521'.red);
+    console.log('Telefon Numarası 10 Haneli Olmalidir. Ex: 5401234521'.red);
     process.exit(1);
 }
 title('Numara: ' + telefon);
 
-let miktar = rl.question("Kac Adet SMS Gondermek Istiyorsunuz: ".green);
-if(isNaN(miktar)) return console.log('Lutfen Bir Rakam Giriniz'.red) && process.exit(1);
+let miktar = rl.question("Kaç Adet SMS Göndermek İstiyorsunuz: ".green);
+if(isNaN(miktar)) return console.log('Lütfen Bir Rakam Giriniz'.red) && process.exit(1);
 if (miktar.length == 0) {
     console.log('Miktar Giriniz'.red);
     process.exit(1);
 }
 title(`Telefon: ${telefon} - Miktar: ${miktar}`);
 
-console.log('SMS Gonderiliyor...'.rainbow);
+console.log('SMS Gönderiliyor...'.rainbow);
 
 fastBomber(telefon, miktar);
